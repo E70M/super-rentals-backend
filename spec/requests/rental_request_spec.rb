80ddb9ba95224cfd14c6a86e60921cc66fde36d4
Eitan Myron <eitan@Air-0271.localdomain>
2020-06-24 11:22:46 -0700

    Test

--- spec/requests/rental_request_spec.rb
+++ spec/requests/rental_request_spec.rb
@@ -2,101 +2,83 @@ require 'rails_helper'
 
 api_headers = { "Content-Type" => "application/vnd.api+json" }
 
-patch_data = {
-  "data": {
-      "id": nil,
-      "type": "rentals",
-      "attributes": {
-          "city": ""
-      }
-    }
-}
-
 RSpec.describe "Rentals" do
   describe 'GET #index' do
     it "returns empty rental list" do
       get "/rentals", { headers: api_headers }
       res_hash = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:success)
       expect(res_hash[:data].count).to eq(0)
     end
   end
 
   describe 'POST #create' do
     it "creates rental" do
-      create(:rental)
-      get "/rentals", { headers: api_headers }
+      rental = create(:rental)
+      get "/rentals/#{rental.id}", { headers: api_headers }
       expect(response).to have_http_status(:success)
     end
+
     it "increases rental count by 1" do
-      # GET all available rentals, find count
-      get "/rentals", { headers: api_headers }
-      res_hash = JSON.parse(response.body, symbolize_names: true)
-      comp_size = res_hash[:data].count + 1
-      create(:rental)
-      # GET all available rentals, check count has increased by 1
-      get "/rentals", { headers: api_headers }
-      res_hash = JSON.parse(response.body, symbolize_names: true)
-      expect(res_hash[:data].count).to eq(comp_size)
+      expect{ create(:rental) }.to change{ Rental.count }.by(1)
     end
+
     it "does not create duplicate rental" do
       # GET all available rentals, find count
       get "/rentals", { headers: api_headers }
       res_hash = JSON.parse(response.body, symbolize_names: true)
       comp_size = res_hash[:data].count + 1
-      # POST same rental twice 
+      # POST same rental twice - expect to raise error
       rental = create(:rental)
       expect{create(:rental, title: rental.title, owner: rental.owner, city: rental.city,
         category: rental.category, bedrooms: rental.bedrooms)}.to raise_error(ActiveRecord::RecordInvalid)
     end
+
     it "does not create rental with missing information" do
       expect{create(:rental, title: "")}.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
   describe 'GET #show' do
+    let!(:rental) { create(:rental) }
+    
     it "returns rental" do
-      create(:rental)
-      # GET all available rentals, select top record for unit id
-      get "/rentals", { headers: api_headers }
-      res_hash = JSON.parse(response.body, symbolize_names: true)
-      unit_id = res_hash[:data][0][:id]
-      get_path = "/rentals/" + unit_id
-      # GET rental with unit id
-      get get_path, { headers: api_headers }
+      get "/rentals/#{rental.id}", { headers: api_headers }
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'PATCH #update' do
     let!(:rental) { create(:rental) }
+    let(:params) do
+      {
+        "data": {
+          "id": rental.id,
+          "type": "rentals",
+          "attributes": {
+            "city": Faker::Address.city
+          }
+        }
+      }
+    end
     before { get "/rentals", { headers: api_headers } }
+
     it "updates rental" do
-      res_hash = JSON.parse(response.body, symbolize_names: true)
-      unit_id = res_hash[:data][0][:id]
-      # Seed req data
-      req = patch_data.deep_dup
-      req[:data][:id] = unit_id
-      req[:data][:attributes][:city] = Faker::Address.city
-      # PATCH rental with unit id, change city name
-      patch_path = "/rentals/" + unit_id
-      patch patch_path, { params: req.to_json, headers: api_headers }
+      patch "/rentals/#{rental.id}", { params: params.to_json, headers: api_headers }
       res_hash = JSON.parse(response.body, symbolize_names: true)
       expect(response).to have_http_status(:success)
+      expect(rental.reload.city).to eq(params[:data][:attributes][:city])
     end
+
     it "does not change rental count" do
       res_hash = JSON.parse(response.body, symbolize_names: true)
-      unit_id = res_hash[:data][0][:id]
       comp_size = res_hash[:data].count
-      # Seed req data
-      req = patch_data.deep_dup
-      req[:data][:id] = unit_id
-      req[:data][:attributes][:city] = Faker::Address.city
       # PATCH rental with unit id, change city name
-      patch_path = "/rentals/" + unit_id
-      patch patch_path, { params: req.to_json, headers: api_headers }
+      patch "/rentals/#{rental.id}", { params: params.to_json, headers: api_headers }
       # GET all available rentals, check count has not changed
       get "/rentals", { headers: api_headers }
       res_hash = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:success)
       expect(res_hash[:data].count).to eq(comp_size)
     end
   end
@@ -104,34 +86,31 @@ RSpec.describe "Rentals" do
   describe 'DELETE #destroy' do
     let!(:rental) { create(:rental) }
     before { get "/rentals", { headers: api_headers } }
+
     it "deletes rental" do
-      res_hash = JSON.parse(response.body, symbolize_names: true)
-      unit_id = res_hash[:data][0][:id]
-      del_path = "/rentals/" + unit_id
-      delete del_path, { headers: api_headers }
-      # GET rental with unit id, check it has been deleted
-      get del_path, { headers: api_headers }
+      delete "/rentals/#{rental.id}", { headers: api_headers }
+      # GET rental by id, check it has been deleted
+      get "/rentals/#{rental.id}", { headers: api_headers }
       expect(response).to have_http_status(:missing)
     end
+
     it "decreases rental count by 1" do
       res_hash = JSON.parse(response.body, symbolize_names: true)
-      unit_id = res_hash[:data][0][:id]
       comp_size = res_hash[:data].count - 1
-      del_path = "/rentals/" + unit_id
-      delete del_path, { headers: api_headers }
+      delete "/rentals/#{rental.id}", { headers: api_headers }
       # GET all available rentals, check collection size has decreased by 1
       get "/rentals", { headers: api_headers }
       res_hash = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:success)
       expect(res_hash[:data].count).to eq(comp_size)
     end
+
     it "does not delete a non-existent record" do
+      delete "/rentals/#{rental.id}", { headers: api_headers }
+      # DELETE same rental - expect to raise error
+      delete "/rentals/#{rental.id}", { headers: api_headers }
       res_hash = JSON.parse(response.body, symbolize_names: true)
-      unit_id = res_hash[:data][0][:id]
-      del_path = "/rentals/" + unit_id
-      delete del_path, { headers: api_headers }
-      # DELETE same rental - expect to return error
-      delete del_path, { headers: api_headers }
-      res_hash = JSON.parse(response.body, symbolize_names: true)
+      expect(response).not_to have_http_status(:success)
       expect(res_hash[:errors][0][:title]).to eq("Record not found")
     end
   end
